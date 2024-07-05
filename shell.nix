239be5a6b01c61a8673ed62baeb0aaeb71fb7d5f
Henner Zeller <h.zeller@acm.org>
2024-07-04 22:53:53 -0700

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,21 @@
+# This is a nix-shell for use with the nix package manager.
+# If you have nix installed, you may simply run `nix-shell`
+# in this repo, and have all dependencies ready in the new shell.
+
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
+  buildInputs = with pkgs;
+    [
+      openscad
+      openscad-lsp
+      pstoedit
+
+      # inspection
+      meshlab
+
+      # CAM
+      prusa-slicer  
+      lightburn
+    ];
+    shellHook = "unset QT_PLUGIN_PATH";
+}
